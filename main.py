cd2b57198d441a94aded3b466845f1172700ef50
JamesMorgenstern <jamesklm@comcast.net>
2022-10-08 13:07:35 -0700

    committed1

--- main.py
+++ main.py
@@ -2,9 +2,16 @@
 
 numbers = [5, 20, 30, 30, 50]
 delval = int(input('Enter the deletion value: '))
+count = 0
+j = 0
+for i in range(len(numbers)):
+    if(delval == numbers[j]):
+        numbers.remove(delval)
+        count += 1
+        j -= 1
+    j += 1
 
-# ******************************
-# Make your Code
-# ******************************
+if (count == 0):
+    numbers.clear()
 
 print (numbers)
